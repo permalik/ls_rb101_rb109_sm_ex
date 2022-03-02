8033791350921d2bdcb74d6280dce3e07ab9210a
Malik <permalik@protonmail.com>
2022-03-01 19:42:16 -0600

    Complete greeting_a_user module

---
+++ greeting_a_user.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+def greeting_a_user
+  puts 'What is your name?'
+  name = gets.chomp
+
+  if name.include?('!')
+    puts "hello #{name}. why are we screaming?".upcase
+  else
+    puts "Hello #{name}."
+  end
+
+  # if name[-1] == '!'
+  #   name = name.chop
+  #   puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+  # else
+  #   puts "Hello #{name}."
+  # end
+
+end
+
+greeting_a_user
