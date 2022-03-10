a514a09b45394a2df348974c6b0e00a02fb6cb67
Malik <permalik@protonmail.com>
2022-03-09 22:47:15 -0600

    Pause LS on consecutive_sum_product module

---
+++ consecutive_sum_product.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+
+
+def consecutive_sum_product
+  puts 'Input an integer greater than zero: '
+  number = gets.chomp.to_i
+
+  puts 'Input compute preference (s/p): '
+  computer = gets.chomp
+
+  number_range = (1..number).to_a
+
+
+end
